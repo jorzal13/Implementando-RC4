bf919d5803c79e917c69e2f52b4c14e88edde5cf
Jairo Ramírez A <aristi_ramirez11@hotmail.com>
2014-03-10 20:34:03 -0500

    Se ordena y actualiza la impresión del PRGA
    En este commit, se ordena la impresión del método Pseudo-Random
    Generation Algorithm (PRGA), haciendo una impresión similar a la
    indicada en http://tools.ietf.org/html/rfc6229,

--- RC4.c
+++ RC4.c
@@ -5,14 +5,14 @@
 
 unsigned char S[256];
 
-
+//Método para intercambiar la posición de una cadena de caracteres
 void swap(unsigned char *s, unsigned int i, unsigned int j) {
     unsigned char temp = s[i];
     s[i] = s[j];
     s[j] = temp;
 }
 
-/* KSA */
+/* KSA Métod para encriptar el mensaje*/
 void rc4_init(unsigned char *key, unsigned int key_length) {
 
 	int i, j;
@@ -25,32 +25,39 @@ void rc4_init(unsigned char *key, unsigned int key_length) {
     }
 }
 
-/* PRGA */
+/* PRGA Método para imprimir la encriptación*/
 unsigned char rc4_output(int longitudMensaje) {
 	int i=0,j=0,k;
-	printf("Long %d\n", longitudMensaje);
+	printf("Bits a imprimir: %d\n", longitudMensaje);
 	for(k=0; k<longitudMensaje; k++){
 		if(k%16==0){
-			printf("\n");
-			printf("%d\t", k);	
+                    printf("\n");
+                    printf("DEC %d  HEX %x:  ", k, k);
 		}
     	i = (i + 1) % 256;
     	j = (j + S[i]) % 256;
     	swap(S, i, j);
-		int t=		S[(S[i] + S[j]) % 256];
-		if(t<16)
-		printf("0%x ", t);
-		else printf("%x ", t);
+		int t=	S[(S[i] + S[j]) % 256];
+		if(t<16){
+                    printf("0%x ", t);
+                    if(((k+1)%4)==0)
+                        printf(" ", t);
+                }else{
+                    printf("%x ", t);
+                    if(((k+1)%4)==0)
+                        printf(" ", t);
+                }
 	}
+        return 0;
 }
 
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
 
 int main() {
-	unsigned char k[]={0x01,0x02,0x03,0x04,0x05};
-	printf("Keylen %d\n", (int)ARRAY_SIZE(k));
+	unsigned char k[]={0x01,0x02,0x03,0x04,0x05};   //key de 40 bits
+	printf("Keylen de %d bytes\n", (int)ARRAY_SIZE(k));
 	rc4_init(k, ARRAY_SIZE(k));
-	rc4_output(4112);
-    
-    return 0;
+	rc4_output(4112);       //imprimos hasta el límite que indica la página de comprobación
+        printf("\n");
+        return 0;
 }
